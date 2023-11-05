b6be400ed8704ccc385d843c3825a23802b79198
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-11-05 17:54:45 +0100

    Update

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -7,7 +7,7 @@ return(
 <div className="d-flex justify-content-center mt-5 py-2 bg-info">
 <p>se ne volete sapere di piu seguiteci nei social per restare sempre aggiornati sul meteo:</p>
 <div className="d-flex mx-3 text-white">
-    <navLink><i class="bi bi-instagram me-2"></i></navLink>
+    <NavLink><i class="bi bi-instagram me-2"></i></NavLink>
     <NavLink><i class="bi bi-twitter-x me-2"></i></NavLink>
     <NavLink><i class="bi bi-facebook me-2"></i></NavLink>
     <NavLink><i class="bi bi-youtube"></i></NavLink>
